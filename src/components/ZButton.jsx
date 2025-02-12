e80e551aee86b1048a48ea071552b1212d96cc7c
Sandeep Behl <sandeep@zintlr.com>
2025-02-12 15:00:24 +0530

    demo setup

--- src/components/ZButton.jsx
+++ src/components/ZButton.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const ZButton = () => {
   return (
-    <div>Hello Bro You did it</div>
-  )
-}
+    <div className="text-2xl text-red-500 flex ">
+      
+      <div className="w-1/2 h-64">Hello Bro You did Hello dsdsdsd world it</div>
+    </div>
+  );
+};
 
-export default ZButton
+export default ZButton;
