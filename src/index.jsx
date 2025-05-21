ced90e1acc613fcbe567e6cf9dc967a014ddfc3d
Sandeep Behl <sandeep@zintlr.com>
2025-05-21 13:40:07 +0530

    fixed req

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,3 @@
-// src/index.js
 export { default as ProfileSection } from "./components/ProfileSection.jsx";
 export { default as HelpSupport } from "./components/ProfileOptions/HelpAndSupportSection/HelpSupport.jsx";
 export { default as request_caller } from "./api-handlers/request-handler.js";
-// Add all other component exports here
