71b76f21ae879e7a762633f1bfd641a7ee56bec8
Sandeep Behl <sandeep@zintlr.com>
2025-05-21 13:52:32 +0530

    Req Caller Changes

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloWorld = () => {
+  return <div>This is new COmponent</div>;
+};
+
+export default HelloWorld;
--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,5 @@
 export { default as ProfileSection } from "./components/ProfileSection.jsx";
 export { default as HelpSupport } from "./components/ProfileOptions/HelpAndSupportSection/HelpSupport.jsx";
+export { default as HelloWorld } from "./components/HelloWorld.jsx";
+
 export { default as request_caller } from "./api-handlers/request-handler.js";
