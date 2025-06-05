dd14684139383db2ae6d97dc6188e288d6637218
Sandeep Behl <sandeep@zintlr.com>
2025-06-05 19:00:17 +0530

    Fixed Some Issues

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,2 @@
-// Components
-export * from './components/ProfileOptions/HelpAndSupportSection/HelpSupport';
-export * from './components/ProfileOptions/HelpAndSupportSection/HelpSupportCard';
-export * from './components/ProfileOptions/HelpAndSupportSection/ZintlrDocCard';
-export * from './components/Toast/Toast';
-
-// Helpers
-export { default as copyToClipboard } from './helpers/Items/copy_to_clipboard';
-export { default as useWindowSize } from './helpers/Items/useWindowSize';
-export { default as isBrowser } from './helpers/Items/isBrowser';
-
 export { default as requestCaller } from './api-handlers/request-handler.js';
 export * from './helpers/index.jsx';
