f43f45dc9600fa4a222f635e6aa477b74922592c
Sandeep Behl <sandeep@zintlr.com>
2025-06-06 18:53:29 +0530

    fixed errors

--- src/exports/components/index.js
+++ src/exports/components/index.js
@@ -1,5 +1,5 @@
 export { ToastWrapper, toast } from '../../components/Toast.js';
 export { default as ProfileWrapperCard } from '../../components/Wrappers/ProfileWrapperCard.jsx';
-export { Counter } from '../../components/Counter.js';
+export { Counter } from '../../components/Counter';
 export { Example } from '../../components/Example.js';
 export { default as ExampleCard } from '../../components/ExampleCard.jsx';
