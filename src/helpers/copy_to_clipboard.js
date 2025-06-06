8f59e5328c82dd460cc39b29f7e6c01cf418dba0
Sandeep Behl <sandeep@zintlr.com>
2025-06-06 18:38:30 +0530

    fixed errors

--- src/helpers/copy_to_clipboard.js
+++ src/helpers/copy_to_clipboard.js
@@ -8,7 +8,7 @@ import { isBrowser } from './isBrowser';
  * @param {string} type The type of data being copied (e.g., "Email"). Default value is "Email".
  * @returns {void} This function does not return anything.
  */
-export const copy_to_clipboard = (text, type = 'Email') => {
+export function copy_to_clipboard(text, type = 'Email'){
   if (isBrowser()) {
     navigator.clipboard.writeText(text);
     toast.success(`${type} copied to clipboard`);
