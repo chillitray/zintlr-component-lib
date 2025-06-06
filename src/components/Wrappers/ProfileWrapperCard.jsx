5257de19a4c6f0f4eddbde44c857f644ef7cfcf7
Sandeep Behl <sandeep@zintlr.com>
2025-06-06 18:19:56 +0530

    fixed errors

--- src/components/Wrappers/ProfileWrapperCard.jsx
+++ src/components/Wrappers/ProfileWrapperCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export const ProfileWrapperCard = ({ children, className, id }) => {
-	return (
-		<div id={id} className={`border w-full h-fit bg-white rounded-lg`}>
-			<div className={`p-3 sm:p-7  mb-2 ${className || ""}`}>{children}</div>
-		</div>
-	);
+  return (
+    <div id={id} className={`border w-full h-fit bg-white rounded-lg`}>
+      <div className={`p-3 sm:p-7  mb-2 ${className || ''}`}>{children}</div>
+    </div>
+  );
 };
