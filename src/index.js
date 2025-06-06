efe2587b15bfae81df7ab339cdd8b99b55bfd9ba
Sandeep Behl <sandeep@zintlr.com>
2025-06-06 19:29:43 +0530

    fixed errors

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ export * from './exports/components';
 
 // Export helpers
 export * from './exports/helpers';
+
+export { ProfileWrapperCard } from './components/Wrappers/ProfileWrapperCard';
+export { ExampleCard } from './components/ExampleCard';
